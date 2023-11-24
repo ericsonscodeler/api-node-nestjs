f22fd59ad9710b72208742aa2401f53e70a07c1f
Ericson Scodeler <ericson.br@hotmail.com>
2023-11-24 12:18:53 -0300

    test for create and authenticate student

--- test/cryptography/fake-encrypter.ts
+++ test/cryptography/fake-encrypter.ts
@@ -1,7 +1,7 @@
 import { Encrypter } from '@/domain/forum/application/cryptography/encrypter'
 
-export class FakerEncrypter implements Encrypter {
-  encrypt(payload: Record<string, unknown>): Promise<string> {
+export class FakeEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
     return JSON.stringify(payload)
   }
 }
--- test/cryptography/fake-hasher.ts
+++ test/cryptography/fake-hasher.ts
@@ -1,7 +1,7 @@
 import { HashCompare } from '@/domain/forum/application/cryptography/hash-compare'
 import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
 
-export class FakerHasher implements HashGenerator, HashCompare {
+export class FakeHasher implements HashGenerator, HashCompare {
   async hash(plain: string): Promise<string> {
     return plain.concat('-hashed')
   }
